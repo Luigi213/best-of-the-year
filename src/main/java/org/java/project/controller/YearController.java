f871d256e63ecfbeee1a72374cee8a168da8157d
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-24 15:11:46 +0200

    step 1 done

--- src/main/java/org/java/project/controller/YearController.java
+++ src/main/java/org/java/project/controller/YearController.java
@@ -1,15 +1,19 @@
 package org.java.project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class YearController {
 	
-	@GetMapping("/")
-	@ResponseBody
-	public String sayHello() {
-		return "hello";
+	@GetMapping("/hola")
+	public String sayHelloSp(Model model,
+			@RequestParam(name = "name") String name) {
+		
+		model.addAttribute("name", name);
+		
+		return "index";
 	}
 }

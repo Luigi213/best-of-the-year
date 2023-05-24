0774f11f4e200390bf1e68d31ac6be62469497a2
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-24 16:06:06 +0200

    step 2 done

---
+++ src/main/java/org/java/project/Movie.java
@@ -0,0 +1,24 @@
+package org.java.project;
+
+public class Movie {
+	private int id;
+	private String titolo;
+	
+	public Movie(int id, String titolo) {
+		setId(id);
+		setTitolo(titolo);
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitolo() {
+		return titolo;
+	}
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+}
--- src/main/java/org/java/project/controller/YearController.java
+++ src/main/java/org/java/project/controller/YearController.java
@@ -1,8 +1,14 @@
 package org.java.project.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.java.project.Movie;
+import org.java.project.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -13,6 +19,82 @@ public class YearController {
 			@RequestParam(name = "name") String name) {
 		
 		model.addAttribute("name", name);
+		
+		return "index";
+	}
+	
+
+	private List<Movie> getBestMovies(){
+		List<Movie> ml = new ArrayList<>();
+		
+		ml.add(new Movie(0, "ciao film"));
+		ml.add(new Movie(1, "ciao1 film1"));
+		ml.add(new Movie(2, "ciao2 film2"));
+		
+		return ml;
+	}
+	
+	private List<Song> getBestSongs(){
+		List<Song> sl = new ArrayList<>();
+		
+		sl.add(new Song(0, "ciao canzone"));
+		sl.add(new Song(1, "ciao1 canzone1"));
+		sl.add(new Song(2, "ciao2 canzone2"));
+		return sl;
+	}
+	
+	@GetMapping("/songs")
+	public String listSongs(Model model) {
+		String list = "";
+		for( Song s : getBestSongs()) {
+			list += s.getTitolo() + ","; 
+		}
+		model.addAttribute("name", list);
+		
+		return "index";
+	}
+	
+	@GetMapping("/movies")
+	public String listMovies(Model model) {
+		String list = "";
+		for( Movie m : getBestMovies()) {
+			list += m.getTitolo() + ","; 
+		}
+		model.addAttribute("name", list);
+		
+		return "index";
+	}
+	
+	@GetMapping("/songs/{id}")
+	public String getIdSongs(Model model,
+			@PathVariable("id") int id) {
+		String song = "";
+		for( Song s : getBestSongs()) {
+			if(s.getId() == id) {				
+				song = s.getTitolo(); 
+			}
+			else {
+				song = "nessuna canzone con questo id";
+			}
+		}
+		model.addAttribute("name", song);
+		
+		return "index";
+	}
+	
+	@GetMapping("/movies/{id}")
+	public String getIdMovies(Model model,
+			@PathVariable("id") int id) {
+		String movie = "";
+		for( Movie m : getBestMovies()) {
+			if(m.getId() == id) {				
+				movie = m.getTitolo(); 
+			}
+			else {
+				movie = "nessuna canzone con questo id";
+			}
+		}
+		model.addAttribute("name", movie);
 		
 		return "index";
 	}
